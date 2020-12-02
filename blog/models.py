23f78438e233900049a90b386b3947a1668d327b
navids32 <naaaaaviiiiid@gmail.com>
2020-12-02 09:31:17 +0330

    'updateAdmin'

--- blog/models.py
+++ blog/models.py
@@ -17,8 +17,8 @@ class UserProfile(models.Model):
     avatar = models.FileField(upload_to='files/user_avatar/', null=False, blank=False, validators=[validate_file_extention])
     description = models.CharField(max_length=512, null=False, blank=False)
 
-    def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name
+    # def __str__(self):
+    #     return self.user.first_name + ' ' + self.user.last_name
 
 
 
@@ -30,15 +30,15 @@ class Article(models.Model):
     category = models.ForeignKey('Category', on_delete=models.CASCADE)
     author = models.OneToOneField(UserProfile,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
 
 
 class Category(models.Model):
     title = models.CharField(max_length=128, null=False, blank=False)
     cover = models.FileField(upload_to='files/cateory_cover/', null=False, blank=False, validators=[validate_file_extention])
 
-    def __str__(self):
-        return self.title
+    # def __str__(self):
+    #     return self.title
 
 
