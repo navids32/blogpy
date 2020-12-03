9223fc7eee123a33fd0abd1f3b17c746cff52eb8
navids32 <naaaaaviiiiid@gmail.com>
2020-12-03 11:01:13 +0330

    'updateContact'

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from  django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexPage.as_view(), name='index')
+    path('', views.IndexPage.as_view(), name='index'),
+    path('contact/', views.ContactPage.as_view(), name='contact')
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -15,8 +15,25 @@ class IndexPage(TemplateView):
                 'create_at' : article.create_at.date(),
             })
 
+        promote_data = []
+        all_promote_articles = Article.objects.filter(promote=True)
+        for promote_article in all_promote_articles:
+            promote_data.append({
+                'title': promote_article.title,
+                'avatar': promote_article.author.avatar.url if promote_article.author.avatar else None,
+                'category': promote_article.category.title,
+                'author': promote_article.author.user.first_name + ' ' +promote_article.author.user.last_name,
+                'cover': promote_article.cover.url if promote_article.cover else None,
+                'create_at': promote_article.create_at.date(),
+            })
+
+
         context = {
             'article_data' : article_data,
+            'promote_article_data' : promote_data,
         }
 
         return render(request, 'index.html', context)
+
+class ContactPage(TemplateView):
+    template_name = 'page-contact.html'
